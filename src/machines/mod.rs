00349b0fff027097767f00f6f307ad84dc223570
Vlamonster <juliadijkstra97@gmail.com>
2025-02-25 17:44:04 +0100

    Small correction

--- src/machines/mod.rs
+++ src/machines/mod.rs
@@ -182,7 +182,7 @@ pub trait Overclock {
         // Compute the advised batch size
         if corrected_processing_time <= ticks {
             let advised_batch = (effective_parallels as f64 * (ticks as f64 + 0.99) / corrected_processing_time as f64) as u64;
-            let duration = corrected_processing_time * ((ticks as f64 + 0.99) / corrected_processing_time as f64) as u64;
+            let duration = (corrected_processing_time as f64 * (advised_batch as f64 / effective_parallels as f64)) as u64;
             (advised_batch, duration)
         } else {
             (effective_parallels, corrected_processing_time)
