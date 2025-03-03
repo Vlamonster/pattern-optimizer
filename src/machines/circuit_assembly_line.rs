79216d56660bf91b77f91f7cfa659a9380095fc0
Vlamonster <juliadijkstra97@gmail.com>
2025-03-03 14:37:38 +0100

    Correction CAL

--- src/machines/circuit_assembly_line.rs
+++ src/machines/circuit_assembly_line.rs
@@ -1,15 +1,15 @@
 use crate::machines::Overclock;
+use crate::model::Recipe;
 use crate::optimization_request::MachineConfiguration;
 
 pub struct CircuitAssemblyLine();
 
 impl Overclock for CircuitAssemblyLine {
-    fn max_parallels(
+    fn perfect_overclocks(
         &self,
-        _parallels_offset: u64,
-        _parallels_per_tier: u64,
-        _tier: u64,
         _machine: &MachineConfiguration,
+        _recipe: &Recipe,
+        _tier: u64,
     ) -> u64 {
         u64::MAX
     }
