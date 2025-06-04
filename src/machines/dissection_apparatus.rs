ba226c5821c54f5a471d96d8fd4e68adcc50459a
Vlamonster <juliadijkstra97@gmail.com>
2025-06-04 17:50:11 +0200

    Use `itemPipeCasings` in Dissection Apparatus instead of assuming max tier

--- src/machines/dissection_apparatus.rs
+++ src/machines/dissection_apparatus.rs
@@ -1,9 +1,19 @@
 use crate::machines::Overclock;
+use crate::optimization_request::MachineConfiguration;
 
 pub struct DissectionApparatus();
 
 impl Overclock for DissectionApparatus {
-    const PARALLELS_OFFSET: u64 = 64;
     const SPEED_MODIFIER: f64 = 3.00;
     const ENERGY_MODIFIER: f64 = 0.85;
+
+    fn max_parallels(
+        &self,
+        _parallels_offset: u64,
+        _parallels_per_tier: u64,
+        _tier: u64,
+        machine: &MachineConfiguration,
+    ) -> u64 {
+        machine.item_pipe_casing_tier * 8
+    }
 }
