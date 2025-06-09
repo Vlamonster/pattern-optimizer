cde501e4b25cc8121eb0c7f1b2af60ad8fc11ffc
Vlamonster <juliadijkstra97@gmail.com>
2025-06-09 20:15:34 +0200

    Put machines in alphabetical order in `machines.lua`

--- src/lua/machines.lua
+++ src/lua/machines.lua
@@ -122,170 +122,6 @@ local upgrades = {
 }
 
 local machines = {
-    ["bender"] = {
-        id = "Industrial Material Press",
-        recipes = {"Bending Machine"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["press"] = {
-        id = "Industrial Material Press",
-        recipes = {"Forming Press"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["extruder"] = {
-        id = "Industrial Extrusion Machine",
-        recipes = {"Extruder"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["wiremill"] = {
-        id = "Industrial Wire Factory",
-        recipes = {"Wiremill"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["hammer"] = {
-        id = "Industrial Sledgehammer",
-        recipes = {"Forge Hammer"},
-        energyUsage = 1 * EU[ULV],
-        tier = 4,
-    },
-    ["extractor"] = {
-        id = "Dissection Apparatus",
-        recipes = {"Extractor"},
-        energyUsage = 1 * EU[ULV],
-        itemPipeCasingTier = Tin,
-    },
-    ["solidifier"] = {
-        id = "Fluid Shaper",
-        recipes = {"Fluid Solidifier", "Tool Casting Machine"},
-        energyUsage = 1 * EU[ULV],
-        width = 0, -- The number of width expansions, min = 0, max = 6
-    },
-    ["cutter"] = {
-        id = "Industrial Cutting Factory",
-        recipes = {"Cutting Machine"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["fextractor"] = {
-        id = "Large Fluid Extractor",
-        recipes = {"Fluid Extractor"},
-        energyUsage = 1 * EU[ULV],
-        solenoidTier = MV,
-        coilTier = Cupronickel,
-    },
-    ["macerator"] = {
-        id = "Industrial Maceration Stack",
-        recipes = {"Macerator"},
-        energyUsage = 1 * EU[ULV],
-        tier = 1, -- No upgrade = 0, Upgrade = 2
-    },
-    ["ebf"] = {
-        id = "Blast Furnace",
-        recipes = {"Blast Furnace"},
-        energyUsage = 1 * EU[ULV],
-        coilTier = Cupronickel,
-    },
-    ["mebf"] = {
-        id = "Mega Blast Furnace",
-        recipes = {"Blast Furnace"},
-        energyUsage = 1 * EU[ULV],
-        coilTier = Cupronickel,
-    },
-    ["volcanus"] = {
-        id = "Volcanus",
-        recipes = {"Blast Furnace"},
-        energyUsage = 1 * EU[ULV],
-        coilTier = Cupronickel,
-    },
-    ["freezer"] = {
-        id = "Vacuum Freezer",
-        recipes = {"Vacuum Freezer"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["mvf"] = {
-        id = "Mega Vacuum Freezer",
-        recipes = {"Vacuum Freezer"},
-        energyUsage = 1 * EU[ULV],
-        tier = 0, -- No Subspace Cooling = 0, SpaceTime = 1, Spatially Enlarged = 2, Eternity = 3
-    },
-    ["cryo"] = {
-        id = "Cryogenic Freezer",
-        recipes = {"Vacuum Freezer"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["mixer"] = {
-        id = "Industrial Mixing Machine",
-        recipes = {"Multiblock Mixer"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["engraver"] = {
-        id = "Hyper-Intensity Laser Engraver",
-        recipes = {"Laser Engraver"},
-        energyUsage = 1 * EU[ULV],
-        laserAmperage = 256,
-    },
-    ["centrifuge"] = {
-        id = "Industrial Centrifuge",
-        recipes = {"Multiblock Centrifuge"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["autoclave"] = {
-        id = "Industrial Autoclave",
-        recipes = {"Autoclave"},
-        energyUsage = 1 * EU[ULV],
-        coilTier = Cupronickel,
-        pipeCasingTier = Bronze,
-        itemPipeCasingTier = Tin,
-    },
-    ["mabs"] = {
-        id = "Mega Alloy Blast Smelter",
-        recipes = {"Alloy Blast Smelter"},
-        energyUsage = 1 * EU[ULV],
-        coilTier = Cupronickel,
-        glassTier = HV,
-    },
-    ["assembler"] = {
-        id = "Precise Auto-Assembler MT-3662",
-        recipes = {"Assembler"},
-        energyUsage = 1 * EU[ULV],
-        tier = 0, -- Mk-0 = 0, Mk-I = 1, Mk-II = 2, Mk-III = 3, Mk-IV = 4
-    },
-    ["compressor"] = {
-        id = "Large Electric Compressor",
-        recipes = {"Compressor"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["blackhole"] = {
-        id = "Pseudostable Black Hole Containment Field",
-        recipes = {"Neutronium Compressor", "Compressor"},
-        energyUsage = 1 * EU[ULV],
-        maximumOverclockTier = ULV,
-    },
-    ["electrolyzer"] = {
-        id = "Industrial Electrolyzer",
-        recipes = {"Multiblock Electrolyzer"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["dehydrator"] = {
-        id = "Utupu-Tanuri",
-        recipes = {"Multiblock Dehydrator"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["chembath"] = {
-        id = "Ore Washing Plant",
-        recipes = {"Chemical Bath"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["mcr"] = {
-        id = "Mega Chemical Reactor",
-        recipes = {"Large Chemical Reactor"},
-        energyUsage = 1 * EU[ULV],
-    },
-    ["lathe"] = {
-        id = "Industrial Precision Lathe",
-        recipes = {"Lathe"},
-        energyUsage = 1 * EU[ULV],
-        itemPipeCasingTier = Tin,
-    },
     ["alloysmelter"] = {
         id = "Zyngen",
         recipes = {"Alloy Smelter"},
@@ -299,32 +135,49 @@ local machines = {
         tier = 1,
         width = 7,
     },
-    ["plasmafurnace"] = {
-        id = "High Current Industrial Arc Furnace",
-        recipes = {"Plasma Arc Furnace"},
+    ["assembler"] = {
+        id = "Precise Auto-Assembler MT-3662",
+        recipes = {"Assembler"},
         energyUsage = 1 * EU[ULV],
-        tier = 2,
-        width = 7,
+        tier = 0, -- Mk-0 = 0, Mk-I = 1, Mk-II = 2, Mk-III = 3, Mk-IV = 4
     },
-    ["nactivator"] = {
-        id = "Neutron Activator",
-        recipes = {"Neutron Activator"},
-        height = 4, -- Height is the number of Speeding Pipe Casings
-    },
-    ["implosion"] = {
-        id = "Electric Implosion Compressor",
-        recipes = {"Electric Implosion Compressor"},
+    ["autoclave"] = {
+        id = "Industrial Autoclave",
+        recipes = {"Autoclave"},
         energyUsage = 1 * EU[ULV],
-        tier = 4,
+        coilTier = Cupronickel,
+        pipeCasingTier = Bronze,
+        itemPipeCasingTier = Tin,
     },
-    ["sifter"] = {
-        id = "Large Sifter Control Block",
-        recipes = {"Sifter"},
+    ["bender"] = {
+        id = "Industrial Material Press",
+        recipes = {"Bending Machine"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["blackhole"] = {
+        id = "Pseudostable Black Hole Containment Field",
+        recipes = {"Neutronium Compressor", "Compressor"},
+        energyUsage = 1 * EU[ULV],
+        maximumOverclockTier = ULV,
+    },
+    ["brewery"] = {
+        id = "Big Barrel Brewery",
+        recipes = {"Brewery"},
         energyUsage = 1 * EU[ULV],
     },
     ["cal"] = {
         id = "Circuit Assembly Line",
         recipes = {"Circuit Assembly Line", "Circuit Assembler"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["centrifuge"] = {
+        id = "Industrial Centrifuge",
+        recipes = {"Multiblock Centrifuge"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["chembath"] = {
+        id = "Ore Washing Plant",
+        recipes = {"Chemical Bath"},
         energyUsage = 1 * EU[ULV],
     },
     ["coal"] = {
@@ -333,9 +186,70 @@ local machines = {
         energyUsage = 1 * EU[ULV],
         tier = LV,
     },
-    ["mdt"] = {
-        id = "Mega Distillation Tower",
-        recipes = {"Distillation Tower"},
+    ["coke"] = {
+        id = "Industrial Coke Oven",
+        recipes = {"Coke Oven"},
+        energyUsage = 1 * EU[ULV],
+        tier = 1, -- Heat Resistant = 1, Heat Proof = 2
+    },
+    ["compressor"] = {
+        id = "Large Electric Compressor",
+        recipes = {"Compressor"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["cryo"] = {
+        id = "Cryogenic Freezer",
+        recipes = {"Vacuum Freezer"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["cutter"] = {
+        id = "Industrial Cutting Factory",
+        recipes = {"Cutting Machine"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["dehydrator"] = {
+        id = "Utupu-Tanuri",
+        recipes = {"Multiblock Dehydrator"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["ebf"] = {
+        id = "Blast Furnace",
+        recipes = {"Blast Furnace"},
+        energyUsage = 1 * EU[ULV],
+        coilTier = Cupronickel,
+    },
+    ["electrolyzer"] = {
+        id = "Industrial Electrolyzer",
+        recipes = {"Multiblock Electrolyzer"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["engraver"] = {
+        id = "Hyper-Intensity Laser Engraver",
+        recipes = {"Laser Engraver"},
+        energyUsage = 1 * EU[ULV],
+        laserAmperage = 256,
+    },
+    ["extractor"] = {
+        id = "Dissection Apparatus",
+        recipes = {"Extractor"},
+        energyUsage = 1 * EU[ULV],
+        itemPipeCasingTier = Tin,
+    },
+    ["extruder"] = {
+        id = "Industrial Extrusion Machine",
+        recipes = {"Extruder"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["fextractor"] = {
+        id = "Large Fluid Extractor",
+        recipes = {"Fluid Extractor"},
+        energyUsage = 1 * EU[ULV],
+        solenoidTier = MV,
+        coilTier = Cupronickel,
+    },
+    ["freezer"] = {
+        id = "Vacuum Freezer",
+        recipes = {"Vacuum Freezer"},
         energyUsage = 1 * EU[ULV],
     },
     ["gorge1"] = {
@@ -352,11 +266,36 @@ local machines = {
         rings = 1,
         upgrades = upgrades,
     },
-    ["smelter"] = {
-        id = "Multi Smelter",
-        recipes = {"Multi Smelter"},
+    ["hammer"] = {
+        id = "Industrial Sledgehammer",
+        recipes = {"Forge Hammer"},
+        energyUsage = 1 * EU[ULV],
+        tier = 4,
+    },
+    ["implosion"] = {
+        id = "Electric Implosion Compressor",
+        recipes = {"Electric Implosion Compressor"},
+        energyUsage = 1 * EU[ULV],
+        tier = 4,
+    },
+    ["lathe"] = {
+        id = "Industrial Precision Lathe",
+        recipes = {"Lathe"},
+        energyUsage = 1 * EU[ULV],
+        itemPipeCasingTier = Tin,
+    },
+    ["mabs"] = {
+        id = "Mega Alloy Blast Smelter",
+        recipes = {"Alloy Blast Smelter"},
         energyUsage = 1 * EU[ULV],
         coilTier = Cupronickel,
+        glassTier = HV,
+    },
+    ["macerator"] = {
+        id = "Industrial Maceration Stack",
+        recipes = {"Macerator"},
+        energyUsage = 1 * EU[ULV],
+        tier = 1, -- No upgrade = 0, Upgrade = 2
     },
     ["magnet"] = {
         id = "Magnetic Flux Exhibitor",
@@ -364,15 +303,76 @@ local machines = {
         energyUsage = 1 * EU[ULV],
         tier = 1, -- Iron = 1, Steel = 2, Neodymium = 3, Samarium = 4, Tengam = 5
     },
-    ["coke"] = {
-        id = "Industrial Coke Oven",
-        recipes = {"Coke Oven"},
+    ["mcr"] = {
+        id = "Mega Chemical Reactor",
+        recipes = {"Large Chemical Reactor"},
         energyUsage = 1 * EU[ULV],
-        tier = 1, -- Heat Resistant = 1, Heat Proof = 2
     },
-    ["brewery"] = {
-        id = "Big Barrel Brewery",
-        recipes = {"Brewery"},
+    ["mdt"] = {
+        id = "Mega Distillation Tower",
+        recipes = {"Distillation Tower"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["mebf"] = {
+        id = "Mega Blast Furnace",
+        recipes = {"Blast Furnace"},
+        energyUsage = 1 * EU[ULV],
+        coilTier = Cupronickel,
+    },
+    ["mixer"] = {
+        id = "Industrial Mixing Machine",
+        recipes = {"Multiblock Mixer"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["mvf"] = {
+        id = "Mega Vacuum Freezer",
+        recipes = {"Vacuum Freezer"},
+        energyUsage = 1 * EU[ULV],
+        tier = 0, -- No Subspace Cooling = 0, SpaceTime = 1, Spatially Enlarged = 2, Eternity = 3
+    },
+    ["nactivator"] = {
+        id = "Neutron Activator",
+        recipes = {"Neutron Activator"},
+        height = 4, -- Height is the number of Speeding Pipe Casings
+    },
+    ["plasmafurnace"] = {
+        id = "High Current Industrial Arc Furnace",
+        recipes = {"Plasma Arc Furnace"},
+        energyUsage = 1 * EU[ULV],
+        tier = 2,
+        width = 7,
+    },
+    ["press"] = {
+        id = "Industrial Material Press",
+        recipes = {"Forming Press"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["sifter"] = {
+        id = "Large Sifter Control Block",
+        recipes = {"Sifter"},
+        energyUsage = 1 * EU[ULV],
+    },
+    ["smelter"] = {
+        id = "Multi Smelter",
+        recipes = {"Multi Smelter"},
+        energyUsage = 1 * EU[ULV],
+        coilTier = Cupronickel,
+    },
+    ["solidifier"] = {
+        id = "Fluid Shaper",
+        recipes = {"Fluid Solidifier", "Tool Casting Machine"},
+        energyUsage = 1 * EU[ULV],
+        width = 0, -- The number of width expansions, min = 0, max = 6
+    },
+    ["volcanus"] = {
+        id = "Volcanus",
+        recipes = {"Blast Furnace"},
+        energyUsage = 1 * EU[ULV],
+        coilTier = Cupronickel,
+    },
+    ["wiremill"] = {
+        id = "Industrial Wire Factory",
+        recipes = {"Wiremill"},
         energyUsage = 1 * EU[ULV],
     },
 }
